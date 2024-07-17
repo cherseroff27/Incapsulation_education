421384f9b680a8bbff2c20f884a77b760cacb0f5
cherseroff <ganstaseriy@gmail.com>
2024-07-17 18:32:40 +0300

    simple code

--- Program.cs
+++ Program.cs
@@ -8,8 +8,12 @@ namespace Incapsulation
 {
     class Program
     {
+        static int number = 10;
         static void Main(string[] args)
         {
+            Console.WriteLine(number);
+
+            Console.ReadLine();
         }
     }
 }
